56318b37cfcef5b069e1646996bf4b053d6377d7
Bel9sh <129883094+Bel9shik@users.noreply.github.com>
2023-04-26 02:59:42 +0700

    Update testlab12.c

--- testlab12.c
+++ testlab12.c
@@ -10,48 +10,15 @@ typedef struct TTestPackage TTestPackage;
 typedef struct TTestInOut TTestInOut;
 typedef struct TTestStrings TTestStrings;
 
-struct TTestStrings{
-    char string [9];
-};
-
-struct TTestPackage {
-    TTestStrings Input[9];
-    TTestStrings Result[9];
-//    size_t CountStrings;
-    size_t ResultCountStrings;
-};
-
-struct TTestInOut{
-    TTestPackage Tests [TESTS_COUNT];
+static const struct {const char *const in; /*int CountResultRows;*/const char *const out;} testInOut[] = {
+        {"..3.2.6..\n9..3.5..1\n..18.64..\n..81.29..\n7.......8\n..67.82..\n..26.95..\n8..2.3..9\n..5.1.3..\n", /*9,*/"483921657\n967345821\n251876493\n548132976\n729564138\n136798245\n372689514\n814253769\n695417382"},
+        {"..18....5\n4...35.2.\n.3.7.....\n..6....4.\n.8..91..2\n...5.....\n....7....\n.9..23..1\n8.....9..\n", /*9,*/"261849735\n479635128\n538712496\n156287349\n784391652\n923564817\n315978264\n697423581\n842156973"},
+        {"61..1.8.3\n.47.8.5..\n...9....6\n...3...14\n324.16.58\n56.498.72\n.365.9...\n....67..5\n7.....4..\n", /*1,*/ "Incorrectly input"}
 };
 
 struct IOStream {
     FILE* In;
     FILE* Out;
-};
-
-static TTestInOut TestData = {
-        {{{
-            {"..3.2.6..",}, {"9..3.5..1"}, {"..18.64.."}, {"..81.29.."}, {"7.......8"}, {"..67.82.."}, {"..26.95.."},{"8..2.3..9"}, {"..5.1.3.."}
-        },
-          {
-            {"483921657"},{"967345821"}, {"251876493"},{"548132976"}, {"729564138"},{"136798245"},{"372689514"},{"814253769"},{"695417382"}},
-            9
-        },
-         {{
-                  {"..18....5"},{"4...35.2."},{".3.7....."},{"..6....4."},{".8..91..2"},{"...5....."},{"....7...."},{".9..23..1"},{"8.....9.."}
-
-         },{
-                 {"261849735"},{"479635128"},{"538712496"},{"156287349"},{"784391652"},{"923564817"},{"315978264"},{"697423581"},{"842156973"}
-         }, 9
-         },
-//         {{
-//                  {"61....8.3"},{".47.8.5.."},{"...9....6"},{"...3...14"},{"324.16.58"},{"56.498.72"},{".365.9..."},{"...a67..5"},{"7.....4.."}
-//         }, {
-//                 {"Incorrectly input"}
-//         }
-//         }
-    }
 };
 
 static size_t currentTest = 0;
@@ -64,14 +31,14 @@ static int FeedFromArray(void){
         printf("Can't create the file. Don't space on disk ?");
         return -1;
     }
-    for (size_t i = 0; i < 9; i++){
-        fprintf(stream.In, "%s\n", TestData.Tests[currentTest].Input[i].string);
+    if(fprintf(stream.In, "%s\n", testInOut[currentTest].in) < 0){
+        printf("Can't create in.txt. No space on disk?\n");
+        fclose(stream.In);
+        return -1;
     }
     fclose(stream.In);
     return 0;
 }
-
-size_t readStringCount = 0;
 
 static int CheckFromArray(void){
     stream.Out = fopen("output.txt","r");
@@ -80,26 +47,15 @@ static int CheckFromArray(void){
         currentTest++;
         return -1;
     }
-
-    char OutString[9] = {0};
-    while(!fscanf(stream.Out,"%s",OutString)){
-        if(readStringCount >= TestData.Tests[currentTest].ResultCountStrings){
-            printf("Output is long -- FAILED\n");
-            return 1;
+    char buf1[128] = {0};
+    int ind = 0;
+    while(fscanf(stream.Out,"%c",&buf1[ind]) != 0){ind ++;}
+    for(int i = 0; i < ind; i++){
+        if(testInOut[currentTest].out[i] != buf1[i]){
+            fclose(stream.Out);
+            currentTest++;
         }
-
-        if(TestData.Tests[currentTest].Result[readStringCount].string[0] != OutString[0] || TestData.Tests[currentTest].Result[readStringCount].string[1] != OutString[1] || TestData.Tests[currentTest].Result[readStringCount].string[2] != OutString[2] || TestData.Tests[currentTest].Result[readStringCount].string[3] != OutString[3] || TestData.Tests[currentTest].Result[readStringCount].string[4] != OutString[4] || TestData.Tests[currentTest].Result[readStringCount].string[5] != OutString[5] || TestData.Tests[currentTest].Result[readStringCount].string[6] != OutString[6] || TestData.Tests[currentTest].Result[readStringCount].string[7] != OutString[7] || TestData.Tests[currentTest].Result[readStringCount].string[8] != OutString[8]){
-            printf("Wrong answer -- FAILED");
-            return 1;
-        }
-        readStringCount ++;
     }
-
-        if(readStringCount < TestData.Tests[currentTest].ResultCountStrings){
-            printf("Output is short -- FAILED");
-            return 1;
-        }
-
     printf("PASSED");
     currentTest ++;;
     fclose(stream.Out);
